13d9dd5a708b4415dbc607b2905b0bcffe039a82
Philipp Schuster <phip1611@gmail.com>
2024-01-09 13:40:26 +0100

    nix: add missing dependencies to shell

--- shell.nix
+++ shell.nix
@@ -1,11 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell rec {
   packages = with pkgs; [
+    pkg-config
   ];
 
   buildInputs = with pkgs; [
     xorg.libX11
     xorg.libXcursor
+    libxkbcommon
   ];
 
   LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
