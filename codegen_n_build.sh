050d75adc784d92d1e23813daaf5a70456548a7f
Philipp Schuster <phip1611@gmail.com>
2026-03-01 19:29:31 +0100

    codegen: more robust in CI

--- codegen_n_build.sh
+++ codegen_n_build.sh
@@ -1,22 +1,26 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
 
 # Invokes the codegen project in "./codegen" to generate the crate in
 # "./src". Afterwards it applies Rustfmt to it, Clippy, and builds everything.
 
-set -e
-set -x
-
-# nice "hack" which make the script work, even if not executed from "./"
+# Make sure script can be executed from any PWD
 DIR=$(dirname "$(realpath "$0")")
 cd "$DIR" || exit
 
 echo "This script generates the crate 'noto-sans-mono-bitmap', verifies the build, and applies Rustfmt and clippy afterwards."
 
-# delete all generated raster files from previous run
+# Delete all generated raster files from previous run
 find src/res_rasterized_characters -type f -name "*.txt" -exec rm {} +
 
 cd "codegen" || exit
-cargo update
+if [ "$1" != "--ci" ]; then
+  cargo update
+else
+  echo "Skipping \`cargo update\`"
+fi
 cargo fmt
 cargo test
 RUSTFLAGS="-C target-cpu=native" cargo run --release --bin codegen
@@ -26,7 +30,7 @@ cd ..
 # formatting. Hence, we always execute this step.
 cargo fmt
 
-if [ "$1" != "--skip-checks" ]; then
+if [ "$1" != "--ci" ]; then
   cargo test --doc --features all
   cargo test --all-targets --features all
   cargo clippy --features all  --all-targets
